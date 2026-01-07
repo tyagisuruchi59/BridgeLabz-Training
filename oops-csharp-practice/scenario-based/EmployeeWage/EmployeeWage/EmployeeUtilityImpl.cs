45f7e5589b9875cc7c76320344bcab051d0959e5
tyagisuruchi59 <suruchityagi972@gmail.com>
2026-01-07 16:32:07 +0530

    [Suruchi Tyagi] add

---
+++ oops-csharp-practice/scenario-based/EmployeeWage/EmployeeWage/EmployeeUtilityImpl.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeWage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    internal class EmployeeUtilityImpl : IEmployee
+    {
+        public void CheckAttendance()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+        }
+    }
+}
